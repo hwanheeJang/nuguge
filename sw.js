66d48bfe94be8b07a2d03430fce3773b1a08c905
hwanheeJang <rapturejhh@gmail.com>
2026-05-06 09:47:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nuguge-v6';
+const CACHE = 'nuguge-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-maskable.svg'];
 
 self.addEventListener('install', e => {
